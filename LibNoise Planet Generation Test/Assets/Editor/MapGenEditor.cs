348d33d55473996e63bebdff1033fb46f866f843
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2016-12-29 11:50:31 -0600

    Improved basic UI functionality.
    The program currently only has implementation in the editor, check the
    inspector window on the map generator object to see the actual
    functionality.

--- LibNoise Planet Generation Test/Assets/Editor/MapGenEditor.cs
+++ LibNoise Planet Generation Test/Assets/Editor/MapGenEditor.cs
@@ -9,11 +9,45 @@ public class MapGenEditor : Editor {
     public override void OnInspectorGUI()
     {
         MapGenerator mapGen = (MapGenerator)target;
-        DrawDefaultInspector();
+        if(DrawDefaultInspector())
+        {
+            //error control
+            if (mapGen.mapWidth < 1) mapGen.mapWidth = 1;
+            if (mapGen.mapHeight < 1) mapGen.mapHeight = 1;
+
+            //going through the noise functions
+            for (int i = 0; i < mapGen.noiseFunctions.Length; i++)
+            {
+                //transfers height / width
+                mapGen.noiseFunctions[i].height = mapGen.mapHeight;
+                mapGen.noiseFunctions[i].width = mapGen.mapWidth;
+            
+                
+
+                //seed distribution, lots of i's scattered for fun
+                if (!mapGen.useRandomSeed)
+                {
+                    mapGen.seedValue = mapGen.seed.GetHashCode()*i;
+                }
+
+                else
+                {
+                    mapGen.seedValue = Random.Range(0, 10000000);
+                }
+                mapGen.noiseFunctions[i].seed = mapGen.seedValue + i;
+            }
+
+            //autoupdate function, so far it's really slow, may remove
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
 
         if (GUILayout.Button("Generate"))
         {
             mapGen.GenerateMap();
         }
     }
+    
 }
--- LibNoise Planet Generation Test/Assets/Scripts/MapGenerator.cs
+++ LibNoise Planet Generation Test/Assets/Scripts/MapGenerator.cs
@@ -9,6 +9,11 @@ public class MapGenerator : MonoBehaviour {
 
     public int mapWidth;
     public int mapHeight;
+    public string seed;
+    public bool useRandomSeed = true;
+    public bool autoUpdate = false;
+    [HideInInspector]
+    public int seedValue = 0;
     private Noise2D noiseMap = null;
     public Texture2D[] textures = new Texture2D[3];
     public NoiseFunctions[] noiseFunctions;
@@ -39,11 +44,9 @@ public class MapGenerator : MonoBehaviour {
         }
 
         noiseMap = new Noise2D(mapWidth, mapHeight, baseModule);
-        //noiseMap.GenerateSpherical(-1, 1, -1, 1);  //doesn't do what I thought it would
+        //noiseMap.GenerateSpherical(-1, 1, -1, 1);  //Needs research
         noiseMap.GeneratePlanar(-1, 1, -1, 1, true);  //the 5 argument version is good for sphere's, may use falloff map for poles and generate seperate meshes for icecaps, not sure
-        //noiseMap.GenerateSpherical(-1, 1, -1, 1); //in this position too
-
-        //noiseMap.GenerateCylindrical(-1, 1, -1, 1);
+        
         textures[0] = noiseMap.GetTexture(LibNoise.Unity.Gradient.Grayscale);
         textures[0].Apply();
         MapDisplay display = FindObjectOfType<MapDisplay>();
@@ -57,23 +60,48 @@ public class MapGenerator : MonoBehaviour {
 [System.Serializable]
 public class NoiseFunctions
 {
-    public enum NoiseType { Perlin, Billow, RiggedMultifractal, Voronoi};
+    public enum NoiseType { Perlin, Billow, RiggedMultifractal, Voronoi };
     public float noiseScale = 0.5f;
     public NoiseType type = NoiseType.Perlin;
     public bool enabled = false;
-    public Noise2D noise2d = null;
     public ModuleBase moduleBase;
+    [HideInInspector]
     public int height;
+    [HideInInspector]
     public int width;
-   
+
+    //Billow and Perlin Inputs
+    [Range(0f,20f)]
+    public double frequency; // on all
+    [Range(2.0000000f, 2.5000000f)]
+    public double lacunarity; // on all but voronoi
+    //[HideInInspector]
+    [Range(0f, 1f)]
+    public double persistence;  //Not on Rigged MultiFractal or voronoi
+    //[HideInInspector]
+    [Range(1,18)]
+    public int octaves;//all but voronoi
+    [HideInInspector]
+    public int seed;//yeah all
+    public QualityMode qualityMode;//not on voronoi
+    //Voronoi Inputs also frequency and seed from previous
+    //[HideInInspector]
+    public double displacement;
+    //[HideInInspector]
+    public bool distance;
+
+
+
 
     public void FormMesh()
     {
 
-        if (type == NoiseType.Billow) { moduleBase = new Billow(); noise2d = new Noise2D(height,width,moduleBase); }
-        else if (type == NoiseType.Perlin) { moduleBase = new Perlin(); noise2d = new Noise2D(height, width, moduleBase); }
-        else if (type == NoiseType.Voronoi) { moduleBase = new Voronoi(); noise2d = new Noise2D(height, width, moduleBase); }
-        else if (type == NoiseType.RiggedMultifractal) { moduleBase = new RiggedMultifractal(); noise2d = new Noise2D(height, width, moduleBase); }
+        if (type == NoiseType.Billow) { moduleBase = new Billow(frequency, lacunarity, persistence, octaves, seed, qualityMode);}
+        else if (type == NoiseType.Perlin) { moduleBase = new Perlin(frequency,lacunarity,persistence,octaves,seed,qualityMode);}
+        else if (type == NoiseType.Voronoi) { moduleBase = new Voronoi(frequency,displacement,seed,distance);}
+        else if (type == NoiseType.RiggedMultifractal) { moduleBase = new RiggedMultifractal(frequency,lacunarity,octaves,seed,qualityMode);}
         else moduleBase = null;
+
+
     }
 }
