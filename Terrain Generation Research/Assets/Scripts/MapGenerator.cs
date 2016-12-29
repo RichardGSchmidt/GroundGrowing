34b4e14ebc182bdb635573650a11c18f55d4d7d4
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2016-12-28 22:55:50 -0600

    Map Generator Script
    Unfinished, use of libnoise to generate a planetary body with realistic
    terrain features.

--- Terrain Generation Research/Assets/Scripts/MapGenerator.cs
+++ Terrain Generation Research/Assets/Scripts/MapGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LibNoise.Unity.Generator;
+using LibNoise.Unity.Operator;
 using LibNoise.Unity;
 public class MapGenerator : MonoBehaviour {
 
@@ -50,7 +51,64 @@ public class MapGenerator : MonoBehaviour {
 
     public Perlin MakeContinent()
     {
-        Perlin perlin = new Perlin();
+        int octaves = 14;
+        double persistance = 0.5;
+        QualityMode qualityMode;
+        qualityMode = QualityMode.High;
+        Perlin perlin = new Perlin(continentalFrequency, continentalLacunarity, persistance, octaves,seed, qualityMode);
         return perlin;
     }
+
+    public Curve CurveContinent(Perlin perlin)
+    {
+        Curve curve = new Curve(perlin);
+        curve.Add(-2.0000 + seaLevel, -1.625 + seaLevel);
+        curve.Add(-1.0000 + seaLevel, -1.375 + seaLevel);
+        curve.Add( 0.0000 + seaLevel, -0.375 + seaLevel);
+        curve.Add( 0.0625 + seaLevel,  0.125 + seaLevel);
+        curve.Add( 0.1250 + seaLevel,  0.250 + seaLevel);
+        curve.Add( 0.2500 + seaLevel,  1.000 + seaLevel);
+        curve.Add( 0.5000 + seaLevel,  0.250 + seaLevel);
+        curve.Add( 0.7500 + seaLevel,  0.250 + seaLevel);
+        curve.Add( 1.0000 + seaLevel,  0.500 + seaLevel);
+        curve.Add( 2.0000 + seaLevel,  0.500 + seaLevel);
+        return curve;
+    }
+
+    public Perlin Carver()
+    {
+        double persistance = 0.5;
+        int octaves = 11;
+        double offsetFreq = 4.34375;
+        Perlin perlin = new Perlin(continentalFrequency * offsetFreq, continentalLacunarity, persistance, octaves, seed + 1, QualityMode.Medium);
+        return perlin;
+    }
+
+    public ScaleBias CarverScaler(Perlin perlin)
+    {
+        double scale = 0.375;
+        double bias = 0.625;
+        ScaleBias scaleBias = new ScaleBias(scale, bias, perlin);
+        return scaleBias;
+    }
+
+    public Min CarvedContinent(ScaleBias scaleBias, Curve curve)
+    {
+        Min min = new Min(scaleBias, curve);
+        return min;
+    }
+
+    public Clamp clampedContinent(Min min)
+    {
+        Clamp clamp = new Clamp(-1.0, 1.0, min);
+        return clamp;
+    }
+
+    public Cache cacheContinent(Clamp clamp)
+    {
+        Cache cache = new Cache(clamp);
+        return cache;
+    }
+
+
 }
