199e7ed26de9a9c952ac32a16b1a3aeda87668ba
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-01-03 08:23:15 -0600

    fixed untracked files

---
+++ LibNoise Planet Generation Test/Assets/Scripts/FlatMeshGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LibNoise;
+using LibNoise.Unity;
+using LibNoise.Unity.Generator;
+
+public static class FlatMeshGenerator {
+
+//function to take a noisemap and adjust a flat terrainmesh according to that map.
+    public static void GenerateTerrainMesh(Noise2D  noisemap)
+    {
+        //noisemap.GeneratePlanar();  
+    }
+}
+
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+
+    int triangleIndex;
+
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+
+    }
+    public void AddTriangle (int a, int b, int c)
+    {
+
+    }
+}
