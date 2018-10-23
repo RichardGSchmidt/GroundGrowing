12595471bd465cc980d9e2ddff8a27afb05598e3
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2018-10-23 00:47:11 -0500

    Revert "ContraOctave created."
    This reverts commit f6e882ceb4f410bcff3ce67977b67116038eced3.

---
+++ Assets/Editor/NoiseDesigner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NoiseDesigner : EditorWindow {
+    [MenuItem("NGUI/Noise Designer",false,120)]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(NoiseDesigner));
+    }
+
+
+    private void OnGUI()
+    {
+        MapGenerator mapGen = FindObjectOfType<MapGenerator>();
+        MapGenEditor mGenEditor = (FindObjectOfType<MapGenEditor>());
+
+    }
+}
